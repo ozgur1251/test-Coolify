39bc2b7f8d8c39de501261394f87d712b3ae87db
ozgur1251 <ozgurcalman@gmail.com>
2025-04-11 11:13:29 +0300

    11-25-11-08

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 /// @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-// import critters from 'astro-critters'; // Critters kaldırıldı
+// import critters from 'astro-critters'; // test-Coolify
 import 'dotenv/config'; // Dotenv'i yapılandırma dosyasında doğrudan yükle
 
 // Ortam değişkenine erişim
